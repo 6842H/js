2daf742396d90c06d991d055df83c279ffd86441
6842H <342186659@qq.com>
2019-07-27 13:52:13 +0800

    '2_windows'

--- note.js
+++ note.js
@@ -1,3 +1,13 @@
+/*格式化填充字符串*/
+String.prototype.format= function(){
+    /*将arguments转化为数组（ES5中并非严格的数组）*/
+    var args = Array.prototype.slice.call(arguments);
+    var count=0;
+    /*通过正则替换%s*/
+    return this.replace(/\$/g,function(s,i){
+        return args[count++];
+    });
+};
 
 /*还原post参数*/
 var basic_params = {
@@ -8,6 +18,12 @@ var basic_params = {
 };
 /*延时展开*/
 var mtimeout = 1500;
+
+var notes_panel_html = '' + 
+	"<div id='notes_panel' style='position:fixed; z-index:999; width:50%; height:70%; left:15%; top:10%; background:pink; border-radius:15px; opacity:0.85;'>" +
+	"<button style='float:right; border-radius:2px; background:red; color:white;' onclick='closeExportNotePanel()'>关闭X</button>" +
+	"<textarea style='padding:2%; width:96%;height:96%; background:#eff; color:green;'>$</textarea>" +
+	"</div>";
 
 /*展开或收起笔记*/
 function expandNoteCtrl(e){
@@ -25,7 +41,7 @@ function expandNoteCtrl(e){
 	}]);
 };
 
-/*获取笔记*/
+/*调用接口获取笔记*/
 function getNotes(size=100, is_me=1, lesson_id=null){
 	NoteList.resetParm({
 		'page': 1, 
@@ -39,13 +55,56 @@ function getNotes(size=100, is_me=1, lesson_id=null){
 	setTimeout(function(){document.getElementById('mexpand').click();}, mtimeout);
 };
 
+/*从标签中提取笔记*/
+function collectNotes(){
+	var items = $("ul#NoteList li.item");
+	var notes = '';
+	if(items && items.length>0){
+		console.debug('items.length=',items.length);
+		for(var i in items){
+			if(!items[i].getElementsByClassName)
+				continue;
+			var author = items[i].getElementsByClassName('blue')[0].innerText;
+			var data = items[i].getElementsByClassName('date')[0].innerText;
+			var content = items[i].getElementsByClassName('content')[0].textContent.replace(/收起↑|展开↓/g, '');
+			notes += data+' by '+author+'\n'+content+'\n\n\n\n';
+		}
+	}
+	console.debug('notes=', notes);
+	return notes;
+};
+
+/*关闭笔记内容导出面板*/
+function closeExportNotePanel(){
+	var t = document.getElementById('notes_panel');
+	if(t){t.parentNode.removeChild(t);t=null;}
+};
+
+/*导出笔记到面板*/
+function openExportNote(){
+	document.body.insertAdjacentHTML("afterBegin",notes_panel_html.format(collectNotes()));
+};
+
 /*变色*/
 function changeColor(e){
 	Array.prototype.forEach.apply($("#con_toolstab_2 .listTitle p"),[(current,index) => {
 		current.className = 'fl';
 	}]);
 	e.className = 'fl cur';
-}
+	$("div.listList").css('margin-top', '50px');
+	var export_p = document.getElementById('export_p');
+	if(export_p){
+		export_p.parentNode.removeChild(export_p);
+	}/*else{*/
+		export_p = null;
+		export_p=document.createElement('p');
+		export_p.id='export_p';
+		export_p.addEventListener('click', openExportNote);
+		export_p.innerText="导出";
+		export_p.title='导出当前笔记内容';	
+	/*}*/
+	e.appendChild(export_p);
+};
 
 
 /*加载显示本人当前正在观看的课程笔记*/
@@ -99,9 +158,9 @@ function replaceNotePanel(){
 	'<pp id="mexpand" onclick="expandNoteCtrl(this)" class="fl" style="cursor:pointer;" type="self_def" title="点击展开↓笔记">展开↓</pp>' +
 	/*'<br>' +
 	'<p onclick="showMyNotesInLesson()" class="fl cur" type="mine" title="导出 我的单元笔记，原我的笔记">导出</p><!--我的笔记-->' +
-	'<p onclick="showMyNotesInCourse()" class="fl" type="mine2" title="导出 我的课程笔记">导出</p>' +
+	'<p onclick="showMyNotesInCourse()" class="fl" type="mine2" title="导出s 我的课程笔记">导出s</p>' +
 	'<p onclick="showAllNotesInLesson()" class="fl" type="all" title="导出 全部单元笔记，原全部笔记">导出</p><!--全部笔记-->' +
-	'<p onclick="showAllNotesInCourse()" class="fl" type="all2" title="导出 全部课程笔记">导出</p>' +
+	'<p onclick="showAllNotesInCourse()" class="fl" type="all2" title="导出s 全部课程笔记">导出s</p>' +
 	*/
 	'';
 	$("#con_toolstab_2 .listTitle").html(html);
@@ -114,5 +173,6 @@ function init(){
 	window.M.mlesson_ids = getLessonIds();
 	replaceNotePanel();
 	console.debug('笔记模块已重置');
+	$("#con_toolstab_2 .listTitle p")[0].click();
 };
-init();
+window.onload=init();
